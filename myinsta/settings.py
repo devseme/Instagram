065be4baa296a5b612b8935212be5c55e15cdcb8
devseme <ian.ochenge@student.moringaschool.com>
2021-12-02 08:29:48 +0300

    modified settings for db and .env

--- myinsta/settings.py
+++ myinsta/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o0aoz4zoitgq=+!4b7vd3-y44=%z1vg!6g5kd-o!rregbca8%7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +80,12 @@ WSGI_APPLICATION = 'myinsta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instadb',
-        'USER': 'moringa',
-    'PASSWORD':'moringa',
-     }    
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+        
     }
+}
 
 
 # Password validation
