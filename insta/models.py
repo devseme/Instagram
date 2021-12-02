5ea2077b9c72ffdb5645f9b6f17862942078a6ea
devseme <ian.ochenge@student.moringaschool.com>
2021-12-02 21:29:38 +0300

    created images class,rendered and uploaded successfully

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
+
+
+
+
+class Images(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+   
+    
+    
+
+    def save_images(self):
+        self.save()
+
+     
+    def delete_images(self):
+        self.delete()
+     # update photos
+    # def update_images(self, name, def update_images(self, name, description,category,location):
+    #     self.name = name
+    #     self.description = description
+    #     self.category = category
+    #     self.location =location
+    #     self.save()description,category,location):
+    #     self.name = name
+    #     self.description = description
+    #     self.category = category
+    #     self.location =location
+    #     self.save()
+
+    def _str_(self):
+        return self.name
+        
+
+        
+
+
+   
+    
+    
+  
--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Images
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Myinsta app')
+    
 def index(request):
-    return render(request, 'all-insta/index.html')
+    photo = Images.objects.all().order_by('-id')
+
+    return render(request, 'all-insta/index.html',{'photo':photo})
