ce179905dbf23a5fdd212c92111fddba8c236732
devseme <ian.ochenge@student.moringaschool.com>
2021-12-02 12:14:39 +0300

    started on template rendering and navbar

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,5 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Myinsta app')
+def index(request):
+    return render(request, 'all-insta/index.html')
--- myinsta/urls.py
+++ myinsta/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.conf.urls import url,include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^insta/',include('insta.urls'))
+    # url(r'^admin/', admin.site.urls),
+    url(r'^',include('insta.urls'))
 ]
 
 
