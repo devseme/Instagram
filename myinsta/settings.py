445699aa29f92962cf644aaf37c5dde38c31152f
devseme <ian.ochenge@student.moringaschool.com>
2021-12-07 07:23:44 +0300

    added crispy forms to style profile form

--- myinsta/settings.py
+++ myinsta/settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 LOGIN_REDIRECT_URL = 'index'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
